32fddd4ea0d9e601b948e589f4e140372f970b71
podpah <stream_bars07@icloud.com>
2023-03-07 16:52:15 +0000

    MVP e-mail page

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    messages = [{'title': 'Message One',
-                 'content': 'Message One Content'},
-                {'title': 'Message Two',
-                 'content': 'Message Two Content'}]
-    return render_template('index.html', messages=messages)
+    messages = [
+        {"title": "Message One", "content": "Message One Content"},
+        {"title": "Message Two", "content": "Message Two Content"},
+    ]
+    return render_template("index.html", messages=messages)
 
 
-@app.route('/watchlist/', methods=('GET', 'POST'))
+@app.route("/watchlist/", methods=("GET", "POST"))
 def watchlist():
-    return render_template('watchlist.html')
+    return render_template("watchlist.html")
