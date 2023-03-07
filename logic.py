8c9e1cdfe0b220bea397313d9bec83ce196d59c5
podpah <pedrinhohrodrigues@outlook.com>
2023-03-07 16:52:15 +0000

    MVP e-mail page

--- logic.py
+++ logic.py
@@ -2,6 +2,8 @@ import smtplib
 import ssl
 from dotenv import dotenv_values
 import requests
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 config = dotenv_values(".env")
 
@@ -18,6 +20,7 @@ current = version.json()[0]
 champ_url = f"https://ddragon.leagueoflegends.com/cdn/{current}/data/en_US/champion.json"
 
 fwrota = f"https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key={config['API_KEY']}"
+# print(requests.get(fwrota).json())
 champ_ids = requests.get(fwrota).json()["freeChampionIds"]
 # Champion ids come from fw rota as ints but are nums in DDragon
 for index, champ_num in enumerate(champ_ids):
@@ -46,60 +49,72 @@ def rota_vs_watchlist(wl):
 
 
 def payload_gen(champions):
+    # MIME has different multiparts with different usages, multipart alternative tries to load the last first and if
+    # that doesn't work, it tries to load the other part
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "Free week rotation alert"
+    message["From"] = config["SMTP_EMAIL"]
+
+    urls = []
+
+    for each in champions:
+        each = each.strip().replace("'", "").lower().capitalize()
+        urls.append(f"https://ddragon.leagueoflegends.com/cdn/{current}/img/champion/{each}.png")
+
+    for i, each in enumerate(urls):
+        urls[i] = f'<img style="height:7.5rem;width:7.5rem;" src="{each}">'
+    x = " ".join(urls)
+
     if len(champions) == 1:
         verb = "is"
         champions = "".join(champions)
     else:
         verb = "are"
-        champions = ", ".join(champions)
+        # champions = ", ".join(champions)
+        champions = ", ".join(champions).rsplit(",", 1)
+        champions = " and".join(champions)
 
-    message = f"""\
+    message_html = f"""\
+    <html>
+        <body>
+            <div style="
+            background-image: url(
+            'https://lol-stats.net/uploads/sxpVPO9w4T9YlJfk4hruCobM2lNeYFNVQPnT8dkR.jpeg');
+            text-align:center;
+            min-width:100vh;
+            min-height:100vh;
+            font-family: 'Comic Sans MS', 'Comic Sans', cursive;
+            background-position: top;
+            background-repeat: no-repeat;
+            background-size: 100% auto;
+            margin: -1rem;
+            text-shadow: 0.1rem 0.1rem black;
+            ">
+            <h1 style="color:#e70c50; padding-top:4rem; margin-top:0%;">Free week rotation alert</h1>
+            <br>
+            <div class="images" style="margin:1rem">
+            {x}
+            </div>
+            <p style="margin:6rem; color:white">Hey, this is a reminder from LossGBT that <strong>{champions}</strong>
+             {verb} currently in the free week rotation!</p> 
+             <br> 
+             <p style="color:white">Enjoy your time in the rift Summoner</p> 
+            </div>
+        </body> 
+    </html> """
+
+    message_txt = f"""\
         Subject: Free Week Rotation
 
         Hey, this is a reminder from LossGBT that {champions} {verb} currently in the free week rotation!
 
         Enjoy your time in the rift Summoner"""
 
-    return message
+    part1 = MIMEText(message_txt, "plain")
+    part2 = MIMEText(message_html, "html")
 
-
-def html_payload_gen(champions):
-    if len(champions) == 1:
-        verb = "is"
-        champions = "".join(champions)
-    else:
-        verb = "are"
-        champions = ", ".join(champions)
-
-    urls = []
-
-    for each in champions:
-        urls.append(f"https://ddragon.leagueoflegends.com/cdn/{current}/img/champion/{each}.png")
-    urls = ["https://ddragon.leagueoflegends.com/cdn/13.4.1/img/champion/Kaisa.png",
-            "https://ddragon.leagueoflegends.com/cdn/13.4.1/img/champion/Tristana.png",
-            "https://ddragon.leagueoflegends.com/cdn/13.4.1/img/champion/Sett.png"]
-
-    for i, each in enumerate(urls):
-        urls[i] = f'<img src="{each}">'
-    x = " ".join(urls)
-
-    message = f"""\
-    <html>
-        <style>
-            * {{text-align:center;font-family: "Comic Sans MS", "Comic Sans", cursive;}} 
-            .images{{margin:1rem;}}
-            body{{background-image: url('https://lol-stats.net/uploads/sxpVPO9w4T9YlJfk4hruCobM2lNeYFNVQPnT8dkR.jpeg');
-            background-repeat: no-repeat;background-position: top;}}
-            h2 {{color:#e70c50}}
-        </style>
-        <body>
-            <h2>Free week rotation alert</h2>
-            <br>
-            <div class="images">
-            {x}
-            </div>
-            <p>Hey, this is a reminder from LossGBT that <strong>{champions}</strong> {verb} currently in the free 
-            week rotation!</p> <br> <p>Enjoy your time in the rift Summoner</p> </body> </html> """
+    message.attach(part1)
+    message.attach(part2)
 
     return message
 
@@ -115,7 +130,7 @@ def send_mail(message, receiver_email):
     with smtplib.SMTP("smtp-mail.outlook.com", port) as server:
         server.starttls(context=context)
         server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message)
+        server.sendmail(sender_email, receiver_email, message.as_string())
 
 
 def run_mail_feature(recipient, wl):
@@ -125,3 +140,10 @@ def run_mail_feature(recipient, wl):
     payload = payload_gen(match)
     send_mail(payload, recipient)
     return wl_adjusting(wl, match)
+
+
+# pay = payload_gen(["Diana", " Ziggs", " Cho'Gath", " Aatrox"])
+# pay2 = payload_gen(["Tristana", " Udyr"])
+# print(pay)
+# send_mail(pay, "preston.harry2003@gmail.com")
+# send_mail(pay2, "preston.harry2003@gmail.com")
