cdf524193bb3bcf2e8e83b09915f0ca07c4d658d
haroldboi <preston.harry2003@gmail.com>
2023-03-14 16:02:35 +0000

    adding support for only 6k docs in match DB

--- search.py
+++ search.py
@@ -3,39 +3,38 @@ import requests
 from dotenv import dotenv_values 
 from pymongo import MongoClient
 array = [0,1,2,4,5,6,7,8]
-class Logic:
+class Search:
     riotSession = None
     dSession = None 
     mongo = None
     @staticmethod 
     def imageDownload():
-        Logic.imageDB['runes'].delete_many({})
-        for rune in Logic.runesObject:
+        Search.imageDB['runes'].delete_many({})
+        for rune in Search.runesObject:
             path = rune["iconPath"].lower().split("/")
             path = "/".join(path[5:])
-            image = Logic.dSession.get("https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/perk-images/"+path)
+            image = Search.dSession.get("https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/perk-images/"+path)
             if image.status_code == 200:
-                Logic.imageDB['runes'].insert_one({'id':rune['id'],"image":image.content})
-        Logic.imageDB['item'].delete_many({})
-        for id in Logic.itemsObject['data']:
-            image = Logic.dSession.get(f"http://ddragon.leagueoflegends.com/cdn/{Logic.patch}/img/item/{id}.png")
+                Search.imageDB['runes'].insert_one({'id':rune['id'],"image":image.content})
+        Search.imageDB['item'].delete_many({})
+        for id in Search.itemsObject['data']:
+            image = Search.dSession.get(f"http://ddragon.leagueoflegends.com/cdn/{Search.patch}/img/item/{id}.png")
             if image.status_code == 200:
-                Logic.imageDB['item'].insert_one({'id':id , 'image' : image.content})
-        Logic.imageDB['champIcon'].delete_many({})
-        for id in Logic.champObject['data']:
-            image = Logic.dSession.get(f"http://ddragon.leagueoflegends.com/cdn/{Logic.patch}/img/champion/{id}.png")
+                Search.imageDB['item'].insert_one({'id':id , 'image' : image.content})
+        Search.imageDB['champIcon'].delete_many({})
+        for id in Search.champObject['data']:
+            image = Search.dSession.get(f"http://ddragon.leagueoflegends.com/cdn/{Search.patch}/img/champion/{id}.png")
             if image.status_code == 200:
-                Logic.imageDB['champIcon'].insert_one({'id':id.lower() , 'image' : image.content})
+                Search.imageDB['champIcon'].insert_one({'id':id.lower() , 'image' : image.content})
             else:
                 print(id)
-        Logic.imageDB['summonerSpells'].delete_many({})
-        for spell in Logic.summonerSpellObject:
+        Search.imageDB['summonerSpells'].delete_many({})
+        for spell in Search.summonerSpellObject:
             path = spell["iconPath"].lower().split("/")
-            image = Logic.dSession.get("https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/data/spells/icons2d/"+path[len(path)-1])
+            image = Search.dSession.get("https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/data/spells/icons2d/"+path[len(path)-1])
             if image.status_code == 200:
-                Logic.imageDB['summonerSpells'].insert_one({'id':spell['id'],"image":image.content})
-       
-
+                Search.imageDB['summonerSpells'].insert_one({'id':spell['id'],"image":image.content})
+    
     @staticmethod
     def start_up():
         config = dotenv_values(".env")
@@ -43,20 +42,21 @@ class Logic:
         appData = mongo["data"]
         riotSess = requests.session()
         riotSess.headers.update({'X-RIOT-TOKEN':config['API_KEY']})
-        Logic.riotSession = riotSess
-        Logic.dSession = requests.session()
-        Logic.mongo = mongo
-        Logic.matchData = appData['match']
-        Logic.imageDB = mongo['image']
-        Logic.patch = Logic.dSession.get("https://ddragon.leagueoflegends.com/api/versions.json").json()[0]
-        Logic.itemsObject =  Logic.dSession.get(f"http://ddragon.leagueoflegends.com/cdn/{Logic.patch}/data/en_US/item.json").json()
-        Logic.champObject =  Logic.dSession.get(f"http://ddragon.leagueoflegends.com/cdn/{Logic.patch}/data/en_US/champion.json").json()
-        Logic.runesObject = Logic.dSession.get(f"https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/perks.json").json()
-        Logic.summonerSpellObject = Logic.dSession.get("https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/summoner-spells.json").json()
-        Logic.itemImages = {}
-        Logic.runeImages = {}
-        Logic.champIcons = {}
-        Logic.summonerSpellImages = {}
+        Search.riotSession = riotSess
+        Search.dSession = requests.session()
+        Search.mongo = mongo
+        Search.matchData = appData['match']
+        Search.imageDB = mongo['image']
+        Search.patch = Search.dSession.get("https://ddragon.leagueoflegends.com/api/versions.json").json()[0]
+        Search.itemsObject =  Search.dSession.get(f"http://ddragon.leagueoflegends.com/cdn/{Search.patch}/data/en_US/item.json").json()
+        Search.champObject =  Search.dSession.get(f"http://ddragon.leagueoflegends.com/cdn/{Search.patch}/data/en_US/champion.json").json()
+        Search.runesObject = Search.dSession.get(f"https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/perks.json").json()
+        Search.summonerSpellObject = Search.dSession.get("https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/summoner-spells.json").json()
+        Search.itemImages = {}
+        Search.runeImages = {}
+        Search.champIcons = {}
+        Search.summonerIcons = {}
+        Search.summonerSpellImages = {}
     
     def __init__(self,server,summonerName,type=''):
         self.server = server
@@ -71,6 +71,14 @@ class Logic:
         x=datetime.datetime.now()
         obj =  self.getNextGames(),self.summonerObject,self.rankedStats,(datetime.datetime.now()-x).total_seconds()
         return obj
+    
+    def insertMatch(self,data):
+        numberOfDocs = Search.matchData.count_documents({})
+        if numberOfDocs > 6000:
+            for each in Search.matchData.find({}).limit(numberOfDocs-6000):
+                Search.matchData.delete_one({"metadata.matchId":each.metadata.matchId})
+        Search.matchData.insert_one(data)
+
     def last20WR(self):
         wins = 0
         losses = 0
@@ -83,28 +91,26 @@ class Logic:
             except:
                 print(type(game))
         self.rankedStats.append({"last20":{"wins":wins,"losses":losses,"WR%":(wins/20)*100}})
-        print(self.rankedStats)
+
     def getNextGames(self,type=''):
         if(type != self.type):
             self.type = type
             self.matchesGotten = 0 
         self.matchList = self.getMatchesArray(self.matchesGotten+20,20,self.type)
         self.fullMatchObjects = self.getMatchInfo()
-        self.keyMatchInfo = self.getFullSummonerStatsForMatch()
-        self.myLast20Games = self.getSpecificSummonerStats()
-        self.last20WR()
-        return self.myLast20Games
-        
+        self.keyMatchInfo =  self.getFullSummonerStatsForMatch()
+        return self.getSpecificSummonerStats()
+    
     def getItemName(self,id):
         try:
-            return Logic.itemsObject['data'][id]['name']
+            return Search.itemsObject['data'][id]['name']
         except:
             return 'None'
         
     def summonerObj (self):
-        obj =  Logic.riotSession.get(f"https://{self.server}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{self.summonerName}").json()
+        obj =  Search.riotSession.get(f"https://{self.server}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{self.summonerName}").json()
         try:
-            obj.update({'iconImage':self.summonerIcon(obj['profileIconId']).content})
+            obj.update({'iconImage':self.summonerIcon(obj['profileIconId'])})
         except KeyError:
             raise Exception(obj['status']['message'],obj['status']['status_code'])
         return obj
@@ -112,24 +118,24 @@ class Logic:
     def getItemImage(self,id):
          if id == '0':
             return 'None'
-         elif id not in Logic.itemImages.keys():
-            itemImage = Logic.imageDB['item'].find_one({"id":id},{"_id":0})
-            Logic.itemImages.update({id:itemImage['image']})
+         elif id not in Search.itemImages.keys():
+            itemImage = Search.imageDB['item'].find_one({"id":id},{"_id":0})
+            Search.itemImages.update({id:itemImage['image']})
             return itemImage['image']
          else:
-            return Logic.itemImages[id]
+            return Search.itemImages[id]
         
     def getStatName(self,statObject):
         list = []
         for id in statObject.values():
-            for rune in Logic.runesObject:
+            for rune in Search.runesObject:
                 if rune['id'] == id:
                     list.append({id:rune['name']})
         return list
     def getPrimaryRuneName(self,styleList):
         list = []
         for id in styleList[0]['selections']:
-            for rune in Logic.runesObject:
+            for rune in Search.runesObject:
                 if rune['id'] == id['perk']:
                     list.append({"id":id['perk'] , "name":rune['name']})
         return list
@@ -137,27 +143,27 @@ class Logic:
     def getSecondaryRuneName(self,styleList):
         list = []
         for id in styleList[1]['selections']:
-            for rune in Logic.runesObject:
+            for rune in Search.runesObject:
                 if rune['id'] == id['perk']:
                     list.append({"id":id['perk'] , "name":rune['name']})
         return list
     def getRuneImage(self,id):
-        if id not in Logic.runeImages.keys():
-            runeImage = Logic.imageDB['runes'].find_one({"id":id},{"_id":0})
-            Logic.runeImages.update({id:runeImage['image']})
+        if id not in Search.runeImages.keys():
+            runeImage = Search.imageDB['runes'].find_one({"id":id},{"_id":0})
+            Search.runeImages.update({id:runeImage['image']})
             return runeImage['image']
         else:
             print("already gotten")
-            return Logic.runeImages[id]
+            return Search.runeImages[id]
     def getChampIcon(self,id):
-        if id not in Logic.champIcons.keys():
-            Image = Logic.imageDB['champIcon'].find_one({"id":id},{"_id":0}) 
-            Logic.champIcons.update({id:Image['image']})
+        if id not in Search.champIcons.keys():
+            Image = Search.imageDB['champIcon'].find_one({"id":id},{"_id":0}) 
+            Search.champIcons.update({id:Image['image']})
             return Image['image']
         else:
-            return Logic.champIcons[id]
+            return Search.champIcons[id]
     def rankedWR (self):
-        rankedWins =  Logic.riotSession.get(f"https://{self.server}.api.riotgames.com/lol/league/v4/entries/by-summoner/{self.summonerObject['id']}").json()
+        rankedWins =  Search.riotSession.get(f"https://{self.server}.api.riotgames.com/lol/league/v4/entries/by-summoner/{self.summonerObject['id']}").json()
         list = []
         try:
             for gameMode in rankedWins:
@@ -174,17 +180,17 @@ class Logic:
 
     def getMatchesArray(self,start,count,type):
         self.matchesGotten += count
-        return Logic.riotSession.get(f"https://{self.region}.api.riotgames.com/lol/match/v5/matches/by-puuid/{self.summonerObject['puuid']}/ids?start={start}&count={count}&type={type}").json()
+        return Search.riotSession.get(f"https://{self.region}.api.riotgames.com/lol/match/v5/matches/by-puuid/{self.summonerObject['puuid']}/ids?start={start}&count={count}&type={type}").json()
 
     def getMatchInfo(self):
         list = [] 
         for matchID in self.matchList:
-            search = Logic.matchData.find_one({"metadata.matchId":matchID},{"_id":0}) 
+            search = Search.matchData.find_one({"metadata.matchId":matchID},{"_id":0}) 
             if(search == None):
                 print('calling api')
                 match = self.riotSession.get(f"https://{self.region}.api.riotgames.com/lol/match/v5/matches/{matchID}")
                 if(match.status_code == 200):
-                    Logic.matchData.insert_one(match.json())
+                    self.insertMatch(match.json())
                     list.append(match.json())
                 else:
                     list.append(match.json())
@@ -217,13 +223,11 @@ class Logic:
                         {'id':f"{player['item5']}", 'name': self.getItemName(f"{player['item5']}")},
                         {'id':f"{player['item6']}", 'name': self.getItemName(f"{player['item6']}")},
                         ],
-                        "fullRunes" :player['perks'],
                         "goldEarned" : player['goldEarned'],
                         "runeStats": self.getStatName(player['perks']['statPerks']),
                         "primaryRunes": self.getPrimaryRuneName(player['perks']['styles']),
                         "secondaryRunes": self.getSecondaryRuneName(player['perks']['styles']),
                         "win" : player['win']
-    
                         })
                 list.append(list1)
             except KeyError:
@@ -257,7 +261,10 @@ class Logic:
             if self.server in y:
                 return x
     def summonerIcon(self,id):
-        return Logic.dSession.get(f"http://ddragon.leagueoflegends.com/cdn/{Logic.patch}/img/profileicon/{id}.png")
+        if id not in Search.summonerIcons.keys():
+            image = Search.dSession.get(f"http://ddragon.leagueoflegends.com/cdn/{Search.patch}/img/profileicon/{id}.png").content
+            Search.summonerIcons.update({id:image})
+        return Search.summonerIcons[id]
     
     # def allImageDownloader(self,redownload,overwrite=False):
     #     directories = ['profileIcons','champIcons','items','runes','spells']
@@ -267,7 +274,7 @@ class Logic:
     #         except FileExistsError:
     #             continue
     #     if(redownload):
-    #         for id in Logic.itemsObject['data']:
+    #         for id in Search.itemsObject['data']:
     #             with open(f'./images/items/{id}.png','wb') as f:
     #                 f.write(self.getItemImage(id).content)
     #                 f.close()
